31afe1c8dc898b3441b504e98c61856a8ccea4aa
kingdish <jimmyxu1997@gmail.com>
2018-08-10 00:17:49 +0800

    add test

---
+++ lottery-react/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import web3 from './web3';
+import lottery from './lottery';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {manager: ''};
+  }
+
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call(); //No need to specify the account. Default account is used.
+    this.setState({manager});
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Lottery Contract</h2>
+        <p>This contract is managed by {this.state.manager}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ lottery/contracts/Lottery.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.4.7;
+
+contract Lottery {
+  address public manager;
+  address[] public players;
+
+  constructor () public {
+    manager = msg.sender;
+  }
+
+  function enter() public payable {
+    require(msg.value > 0.01 ether);
+
+    players.push(msg.sender);
+  }
+
+  function random() private view returns (uint) {
+    return uint(keccak256(block.difficulty, now, players));
+  }
+
+  function pickWinner() public restricted {
+    uint index = random() % players.length;
+    players[index].transfer(this.balance);
+    players = new address[](0);
+  }
+
+  function getPlayers() public view restricted returns (address[]) {
+    return players;
+  }
+
+  modifier restricted() {
+    require(msg.sender == manager);
+    _;
+  }
+}
