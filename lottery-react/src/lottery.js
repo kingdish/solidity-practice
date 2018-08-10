945e721ff956b4c2197296eab021a3f3057fb8dc
kingdish <jimmyxu1997@gmail.com>
2018-08-10 09:59:41 +0800

    add interface

--- lottery-react/src/lottery.js
+++ lottery-react/src/lottery.js
@@ -1,58 +1,6 @@
 import web3 from './web3';
 
-const address = '0x9cE14Dcf4C51d000D81Ca7B21Ed6b0dE08EF3595';
-const abi = [
-  {
-    "constant":true,
-    "inputs":[],
-    "name":"manager",
-    "outputs":[{"name":"","type":"address"}],
-    "payable":false,
-    "stateMutability":"view",
-    "type":"function"
-  },
-  {
-    "constant":false,
-    "inputs":[],
-    "name":"pickWinner",
-    "outputs":[],
-    "payable":false,
-    "stateMutability":"nonpayable",
-    "type":"function"
-  },
-  {
-    "constant":true,
-    "inputs":[],
-    "name":"getPlayers",
-    "outputs":[{"name":"","type":"address[]"}],
-    "payable":false,
-    "stateMutability":"view",
-    "type":"function"
-  },
-  {
-    "constant":false,
-    "inputs":[],
-    "name":"enter",
-    "outputs":[],
-    "payable":true,
-    "stateMutability":"payable",
-    "type":"function"
-  },
-  {
-    "constant":true,
-    "inputs":[{"name":"","type":"uint256"}],
-    "name":"players",
-    "outputs":[{"name":"","type":"address"}],
-    "payable":false,
-    "stateMutability":"view",
-    "type":"function"
-  },
-  {
-    "inputs":[],
-    "payable":false,
-    "stateMutability":"nonpayable",
-    "type":"constructor"
-  }
-];
+const address = '0x995629BE29998e63547f88AA54BCEF24AC41DcA8';
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
 
 export default new web3.eth.Contract(abi, address);
