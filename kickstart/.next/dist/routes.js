2cd338b2e7cde53a740d9a9c8aab39adc1f15b56
kingdish <jimmyxu1997@gmail.com>
2018-08-12 22:03:13 +0800

    update next.js

--- kickstart/.next/dist/routes.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-var routes = require('next-routes')();
-
-module.exports = routes;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInJvdXRlcy5qcyJdLCJuYW1lcyI6WyJyb3V0ZXMiLCJyZXF1aXJlIiwibW9kdWxlIiwiZXhwb3J0cyJdLCJtYXBwaW5ncyI6Ijs7QUFBQSxJQUFNLFNBQVMsQUFBZjs7QUFFQSxPQUFPLEFBQVAsVUFBaUIsQUFBakIiLCJmaWxlIjoicm91dGVzLmpzIiwic291cmNlUm9vdCI6Ii9Vc2Vycy9qaW1teXh1L0RvY3VtZW50cy8yMDE4X3N1bW1lcl9pbnRlcm4vZXRoZXJldW0vc29saWRpdHktcHJhY3RpY2Uva2lja3N0YXJ0In0=
